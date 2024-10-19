2f19fe7275de922643500b2885ac8bb2b01309c2
uday-Kiran-manthena <udaymanthana07@gmail.com>
2024-10-19 21:26:33 +0530

    Modified

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,24 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
+
 import { connectDB } from "./db/connectDB.js";
+
 import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.json());
 const __dirname = path.resolve();
 
-app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.get("/", (req, res) => {
-  res.send("HI, Hello ");
-});
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
@@ -29,5 +31,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   connectDB();
-  console.log("Server Started");
+  console.log("Server is running on port: ", PORT);
 });
